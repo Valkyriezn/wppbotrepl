d1acdbb8428b38d0b5dfd2911b4c0b2573963dbe
Valkyriezn <75204590+Valkyriezn@users.noreply.github.com>
2023-05-22 19:52:30 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { autonodecron } from '@utils/cron.utils'
 import { mongodb } from '@utils/database/mongodb'
 import { clearSession } from '@utils/helper.utils'
 import connect from './server'
+import express from 'express'
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
 
 clearSession()
 mongodb(process.env.MONGODB_URL)
